b364e6393cb460d84ed8aae42174440f764fda65
Chris Zuber <shgysk8zer0@gmail.com>
2020-08-03 09:43:54 -0700

    Update contact form submission handling
    - Fix invalid `import { notify } ...`
    - Use `HTMLNotificationElement` instead
    - Do not use `<toast-message>` on submission
    - Add actions and more options to shown notification

--- js/contact-demo.js
+++ js/contact-demo.js
@@ -1,34 +1,69 @@
-import { notify } from 'https://cdn.kernvalley.us/js/std-js/notification-shim.js';
+import { HTMLNotificationElement } from 'https://cdn.kernvalley.us/components/notification/html-notification.js';
+import { $ } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 
 export async function submitHandler(event) {
 	event.preventDefault();
 	const target = event.target;
-	await Promise.all([
-		customElements.whenDefined('toast-message'),
-		new Promise(resolve => {
-			notify('Nothing was sent', {
-				body: 'This is for demonstration purposes only.',
-				icon: '/img/favicon.svg',
-				vibrate: [500, 0, 0, 500],
-				tag: 'demo',
-			}).addEventListener('close', () => resolve());
-		})
-	]);
-	const Toast = customElements.get('toast-message');
-	const toast = new Toast();
-	const pre = document.createElement('pre');
-	const code = document.createElement('code');
 	const data = new FormData(target);
+	$('fieldset, input, button', target).disable();
 
-	pre.slot = 'content';
-	code.textContent = JSON.stringify(Object.fromEntries(data.entries()), null, 4);
+	await new Promise(resolve => {
+		const notification = new HTMLNotificationElement(data.get('name'), {
+			body: 'Nothing submitted. This is for demonstration purposes only.',
+			icon: '/img/favicon.svg',
+			lang: 'en-us',
+			dir: 'ltr',
+			tag: 'demo',
+			silent: false,
+			vibrate: [500, 0, 0, 500],
+			requireInteraction: true,
+			data: {
+				name: data.get('name'),
+				email: data.get('email'),
+				phone: data.get('phone'),
+				subject: data.get('subject'),
+				body: data.get('body'),
+			},
+			actions: [{
+				title: 'Share',
+				action: 'share',
+			}, {
+				title: 'Clear',
+				action: 'clear',
+			}, {
+				title: 'Close',
+				action: 'close',
+			}]
+		});
 
-	pre.append(code);
-	toast.append(pre);
-	toast.backdrop = true;
-	document.body.append(toast);
-	await toast.show();
-	await toast.closed;
-	toast.remove();
-	target.reset();
+		notification.addEventListener('close', () => resolve());
+
+		notification.addEventListener('notificationclick', async ({ action, notification }) => {
+			switch (action) {
+				case 'share':
+					if (navigator.canShare({title: '', text: '', url: location.href})) {
+						const { subject, body } = notification.data;
+						await navigator.share({
+							title: subject,
+							text: body,
+							url: location.href,
+						});
+					} else {
+						alert('Share API not supported');
+					}
+					break;
+
+				case 'clear':
+					target.reset();
+					notification.close();
+					break;
+
+				case 'close':
+					notification.close();
+					break;
+			}
+		});
+	});
+
+	$('fieldset, input, button', target).enable();
 }
