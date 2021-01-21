a4a99a211cb90af5da8c570e067dd7945a050168
Chris Zuber <shgysk8zer0@gmail.com>
2021-01-20 17:06:23 -0800

    Add `keep-kern-clean` into site data

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,6 @@ layout: null
 /* global init: readonly, config: readonly */
 /* {{ site.data.app.version | default: site.version }} */
 const CDN = 'https://cdn.kernvalley.us/';
-// const CDN = 'https://deploy-preview-419--hardcore-bhaskara-69a703.netlify.app/';
 
 try {
 	self.importScripts(new URL('./service-worker.js', CDN), '/sw-config.js');
